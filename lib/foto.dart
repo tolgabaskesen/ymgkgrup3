38ac65cc05fbde4cb7c79bf77894c8e1b7ba3629
Tolga Başkesen <69087561+tolgabaskesen@users.noreply.github.com>
2021-01-06 09:56:05 +0300

    Update foto.dart

--- lib/foto.dart
+++ lib/foto.dart
@@ -363,7 +363,7 @@ class _FotoEkleState extends State<FotoEkle> {
     if (response.statusCode == 200) {
       return Album.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load album');
+      throw Exception('Albüm Yüklenemedi');
     }
   }
 }
